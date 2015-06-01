58a76f1e4e6d457acafe01c6ef8e917901fd1f94
Dmitri Naumov <dmitri.naumov@ufz.de>
2015-06-01 16:49:12 +0200

    Initial skeleton and cmake config.

---
+++ vtkdiff.cpp
@@ -0,0 +1,21 @@
+/**
+ * \copyright
+ * Copyright (c) 2015, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ */
+
+#include <cstdlib>
+
+#include <tclap/CmdLine.h>
+
+#include <vtkXMLUnstructuredGridReader.h>
+
+
+int
+main(int argc, char* argv[])
+{
+
+    return EXIT_SUCCESS;
+}
