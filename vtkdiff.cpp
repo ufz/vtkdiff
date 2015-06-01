fde38715ecde32f7b4192598dc8cffd84441ab54
Dmitri Naumov <dmitri.naumov@ufz.de>
2015-06-01 19:07:17 +0200

    Calculate errors and their norms.
    After prelimenary checks calculate tuple-wise
    errors.
    
    In particular for the absolute and the relative
    errors the l1-norm, l2-norm (and squared),
    maximum norm.
    The relative error is:
    0 if absolute error is 0,
    inf if absolute error != 0 and array a value is 0,
    abs_err / | a_i | else,
    as in numdiff.

--- vtkdiff.cpp
+++ vtkdiff.cpp
@@ -6,11 +6,13 @@
  *              http://www.opengeosys.org/project/license
  */
 
+#include <algorithm>
 #include <cstdlib>
 
 #include <tclap/CmdLine.h>
 
 #include <vtkDataArray.h>
+#include <vtkDoubleArray.h>
 #include <vtkPointData.h>
 #include <vtkSmartPointer.h>
 #include <vtkUnstructuredGrid.h>
@@ -84,6 +86,100 @@ main(int argc, char* argv[])
             << "not found in point data.\n";
         return EXIT_FAILURE;
     }
+
+    // Check similarity of the data arrays.
+
+    // Is numeric
+    if (!a->IsNumeric())
+    {
+        std::cerr << "Data in data array a is not numeric:\n"
+            << "data type is " << a->GetDataTypeAsString() << "\n";
+
+        return EXIT_FAILURE;
+    }
+    if (!b->IsNumeric())
+    {
+        std::cerr << "Data in data array b is not numeric.\n"
+            << "data type is " << b->GetDataTypeAsString() << "\n";
+        return EXIT_FAILURE;
+    }
+
+    // Number of components
+    if (a->GetNumberOfComponents() != b->GetNumberOfComponents())
+    {
+        std::cerr << "Number of components differ:\n"
+            << a->GetNumberOfComponents() << " in data array a and "
+            << b->GetNumberOfComponents() << " in data array b\n";
+        return EXIT_FAILURE;
+    }
+
+    // For now only scalar type arrays are allowed.
+    if (a->GetNumberOfComponents() != 1)
+    {
+        std::cerr << "Only scalar data arrays are supported.\n";
+        return EXIT_FAILURE;
+    }
+
+    // Calculate difference of the data arrays.
+
+    // Absolute error and norms.
+    double abs_err_norm_l1 = 0;
+    double abs_err_norm_2_2 = 0;
+    double abs_err_norm_max = 0;
+    vtkSmartPointer<vtkDoubleArray> abs_err = vtkDoubleArray::New();
+    abs_err->SetNumberOfComponents(a->GetNumberOfComponents());
+    abs_err->SetNumberOfTuples(a->GetNumberOfTuples());
+
+    // Relative error and norms.
+    double rel_err_norm_l1 = 0;
+    double rel_err_norm_2_2 = 0;
+    double rel_err_norm_max = 0;
+    vtkSmartPointer<vtkDoubleArray> rel_err = vtkDoubleArray::New();
+    rel_err->SetNumberOfComponents(a->GetNumberOfComponents());
+    rel_err->SetNumberOfTuples(a->GetNumberOfTuples());
+
+    for (vtkIdType i = 0; i < a->GetNumberOfTuples(); ++i)
+    {
+        // absolute error and its norms:
+        abs_err->SetTuple1(i, a->GetTuple1(i) - b->GetTuple1(i));
+        double const abs_err_i = abs_err->GetTuple1(i);
+
+        abs_err_norm_l1 += abs_err_i;
+        abs_err_norm_2_2 += abs_err_i*abs_err_i;
+        abs_err_norm_max = std::max(abs_err_norm_max, abs_err_i);
+
+        // relative error (to the data array a) and its norms:
+        double const abs_a_i = std::abs(a->GetTuple1(i));
+        if (abs_err_i == 0)
+        {
+            rel_err->SetTuple1(i, 0);
+        }
+        else if (abs_a_i == 0)
+        {
+            rel_err->SetTuple1(i, std::numeric_limits<double>::infinity());
+        }
+        else
+        {
+            rel_err->SetTuple1(i, abs_err_i / abs_a_i);
+        }
+        double const rel_err_i = rel_err->GetTuple1(i);
+
+        rel_err_norm_l1 += rel_err_i;
+        rel_err_norm_2_2 += rel_err_i*rel_err_i;
+        rel_err_norm_max = std::max(rel_err_norm_max, rel_err_i);
+    }
+
+    // Error information
+    std::cout << "Computed difference between data arrays:\n"
+        << "abs l1 norm = " << abs_err_norm_l1 << "\n"
+        << "abs 2-norm = " << abs_err_norm_2_2 << "\n"
+        << "abs 2-norm^2 = " << std::sqrt(abs_err_norm_2_2) << "\n"
+        << "abs maximum norm = " << abs_err_norm_max << "\n"
+        << "\n"
+        << "rel l1 norm = " << rel_err_norm_l1 << "\n"
+        << "rel 2-norm = " << rel_err_norm_2_2 << "\n"
+        << "rel 2-norm^2 = " << std::sqrt(rel_err_norm_2_2) << "\n"
+        << "rel maximum norm = " << rel_err_norm_max << "\n";
 
     return EXIT_SUCCESS;
 }
